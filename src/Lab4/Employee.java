cf06bb991678015f6f63f993a37e8a7cd434e1ce
Christian <ctschleif@gmail.com>
2017-02-09 11:47:37 -0600

    added documentation!

--- src/Lab4/Employee.java
+++ src/Lab4/Employee.java
@@ -1,27 +1,46 @@
 package Lab4;
 
 /**
- * Created by Christian on 2/7/2017.
+ * @author Christian Schleif
+ * Last modified 9 Feb 2017
+ * A class representing an employee. Implements interface Rules
  */
 public class Employee implements Rules {
     private String name = "";
     private int salary = 0;
 
+    /**
+     * Employee constructor including individual's name and salary
+     * @param inName The employee's name
+     * @param inSalary The employee's salary
+     */
     public Employee (String inName, int inSalary){
         this.name = inName;
         this.salary = inSalary;
     }
 
+    /**
+     * Get the salary of this employee
+     * @return The salary
+     */
     @Override
     public int getSalary() {
         return salary;
     }
 
+    /**
+     * Get the name of the employee
+     * @return The name
+     */
     @Override
     public String getName() {
         return name;
     }
 
+    /**
+     * Get the representation of the employee, with name and salary
+     * @return A string of name and salary for the employee.
+     */
     @Override
     public String toString(){
         return name + " has a salary of " + salary;
--- src/Lab4/Faculty.java
+++ src/Lab4/Faculty.java
@@ -1,46 +1,83 @@
 package Lab4;
 
 /**
- * Created by Christian on 2/7/2017.
+ * @author Christian Schleif
+ * Last modified 2/9/2017
+ *
+ * Faculty class, inheriting employee, represents an individual with a course collection
  */
 public class Faculty extends Employee {
     private String[] courses = new String[0];
-    public Faculty(String name, int salary, String[] courses){
+
+    /**
+     * Faculty constructor with information
+     *
+     * @param name    The name of the faculty member
+     * @param salary  The salary of the individual
+     * @param courses The courses the individual teaches
+     */
+    public Faculty(String name, int salary, String[] courses) {
         super(name, salary);
         this.setCourses(courses);
     }
 
+    /**
+     * Get the name of the faculty member.
+     *
+     * @return The name, prefixed with title Professor
+     */
     @Override
     public String getName() {
         return "Professor " + super.getName();
     }
 
-    public String[] getCourses(){
+    /**
+     * Get a copy of the courses taught by the professor
+     *
+     * @return A copy array of strings representing courses
+     */
+    public String[] getCourses() {
         String[] output = new String[courses.length];
-        for(int i = 0; i < output.length; i++){
+        for (int i = 0; i < output.length; i++) {
             output[i] = courses[i];
         }
         return output;
     }
 
-    public void setCourses(String[] newCourses){
+    /**
+     * Set the courses that this professor instructs in, making a copy to be safe.
+     *
+     * @param newCourses The (threadsafe? copysafe?) array to pass in as the professor course list
+     */
+    public void setCourses(String[] newCourses) {
         // ok we want a copy?
-        courses=  new String[newCourses.length];
-        for(int i = 0; i < courses.length; i++){
+        courses = new String[newCourses.length];
+        for (int i = 0; i < courses.length; i++) {
             courses[i] = newCourses[i];
         }
     }
 
+    /**
+     * Represent the faculty member in a succinct string.
+     *
+     * @return The representation including course collection.
+     */
     @Override
-    public String toString(){
+    public String toString() {
         return super.toString() + " -- Has courses: " + commaJoin(this.courses);
     }
 
-    private String commaJoin(String[] arr){
+    /**
+     * Join an array of strings with a comma
+     *
+     * @param arr The array of strings (unmodified)
+     * @return The string made by combining the strings.
+     */
+    private String commaJoin(String[] arr) {
         String output = "";
-        for(int i = 0; i < arr.length; i++){
+        for (int i = 0; i < arr.length; i++) {
             output += arr[i] + ",";
         }
-        return output.substring(0, output.length()-1);
+        return output.substring(0, output.length() - 1);
     }
 }
